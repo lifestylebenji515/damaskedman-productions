d6a7676cd987d28cf29ece3543de9cf8b5ff8a62
stackfiendllc-debug <stackfiendllc@gmail.com>
2026-05-29 14:27:17 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
-
-  setTimeout(() => {
-    loader.classList.add("hidden");
-    document.body.classList.add("loaded");
-  }, 2500);
-});
