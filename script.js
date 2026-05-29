6ddd772e519e4cc3a8559ba0575850611d59eec1
stackfiendllc-debug <stackfiendllc@gmail.com>
2026-05-29 13:40:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+window.onload = function () {
+  setTimeout(() => {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("main-content").classList.remove("hidden");
+  }, 2500);
+};
